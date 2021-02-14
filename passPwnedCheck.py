ff027490114a05eeca25b9e0b4cdb876dfa34b93
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2021-02-14 14:24:08 -0600

    Adding comments to passPwnedCheck.py

--- passPwnedCheck.py
+++ passPwnedCheck.py
@@ -1,5 +1,10 @@
 # passPwnedCheck.py utilizes the API from haveibeenpwned.com; which contains over 1/2 billion passwords
 # These password that have been exposed in previous breaches and stored in SHA-1 hash
 # Details of the site/API can be found under https://haveibeenpwned.com/API/v3#PwnedPasswords
-#   
+#
+# pasPwnedCheck.py does not expose the password or its hash to haveibeenpwned.com
+# As they implement a k-Anonymity model; which allows querying by partial hash only
+#
+
+
 
